cc5a6614c4bc12c32e6e4ca66b09482b016fb6c2
DaehoSon <thseogh135@gmail.com>
2023-11-21 02:34:14 +0900

    done: Lv 1/완주하지 못한 선수

--- Programmers/Lv_1/완주하지_못한_선수/solution.py
+++ Programmers/Lv_1/완주하지_못한_선수/solution.py
@@ -1,12 +1,20 @@
+# 2
 from collections import Counter
 
 
 def solution(participant, completion):
-    answer = ''
-    participant_counter = dict(Counter(participant))
-    for c in completion:
-        participant_counter[c] -= 1
-    for player, count in participant_counter.items():
-        if count != 0:
-            answer = player
-    return answer
+    return list((Counter(participant) - Counter(completion)).keys())[0]
+
+# 1
+# from collections import Counter
+#
+#
+# def solution(participant, completion):
+#     answer = ''
+#     participant_counter = dict(Counter(participant))
+#     for c in completion:
+#         participant_counter[c] -= 1
+#     for player, count in participant_counter.items():
+#         if count != 0:
+#             answer = player
+#     return answer
