f311ac30f261a775564d4f3d158455311e216401
DaehoSon <thseogh135@gmail.com>
2024-02-01 12:16:47 +0900

    add: Lv3/기지국 설치

--- Programmers/python/Lv_3/기지국_설치/solution.py
+++ Programmers/python/Lv_3/기지국_설치/solution.py
@@ -1,4 +1,12 @@
 def solution(n, stations, w):
     answer = 0
+    w_range = 2 * w + 1
+    prev_end_w = 0
+    stations.append(n + 1 + w)
+    for station in stations:
+        current_start_w = station - w
+        current_end_w = station + w
+        _div, _mod = divmod(current_start_w - prev_end_w - 1, w_range)
+        answer += _div if not _mod else _div + 1
+        prev_end_w = current_end_w
     return answer
-œ
